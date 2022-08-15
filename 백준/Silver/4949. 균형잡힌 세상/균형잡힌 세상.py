f4e8f1da06f2c4ebf2bcbf266b329a4c41a8ae43
kkeunn <96153290+kkeunn@users.noreply.github.com>
2022-08-15 23:28:50 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 656 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,25 @@
+while True :
+    sentence = input()
+    stack = []
+    if sentence == "." :
+        break
+
+    for s in sentence :
+        if s == '[' or s == '(' :
+            stack.append(s)
+        elif s == ']' :
+            if stack and stack[-1] == '[' :
+                stack.pop()
+            else : 
+                stack.append(']')
+                break
+        elif s == ')' :
+            if stack and stack[-1] == '(' :
+                stack.pop()
+            else :
+                stack.append(')')
+                break
+    if not stack :
+        print('yes')
+    else :
+        print('no')
