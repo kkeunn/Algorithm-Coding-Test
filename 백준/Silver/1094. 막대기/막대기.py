a09f8b84c16edd2c2f84b2bfc0152b56c84b4245
kkeunn <96153290+kkeunn@users.noreply.github.com>
2022-09-02 23:31:33 +0900

    [Silver V] Title: 막대기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -0,0 +1,14 @@
+import sys
+
+X = int(sys.stdin.readline().rstrip())
+stick = [64, 32, 16, 8, 4, 2, 1]
+count = 0
+for i in range(len(stick)):
+    if X >= stick[i]:
+        count += 1
+        X -= stick[i]
+
+    if X == 0:
+        break
+
+print(count)
