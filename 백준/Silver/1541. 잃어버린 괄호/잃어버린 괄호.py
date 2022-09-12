a51745e793c4f728891a137e3899a84f8afe843d
kkeunn <96153290+kkeunn@users.noreply.github.com>
2022-09-12 23:58:14 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,12 @@
+import sys
+
+exp = sys.stdin.readline().split("-")
+ans = 0
+for i in exp[0].split("+"):
+    ans += int(i)
+
+for i in exp[1:]:
+    for j in i.split("+"):
+        ans -= int(j)
+
+print(ans)
